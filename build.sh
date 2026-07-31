25f45db1f41c8b64c82fd77316300b616b29ee28
Guru901 <avinashkaur842@gmail.com>
2026-07-31 05:33:49 +0530

    Add error messages in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+
+if ! command -v qemu-system-x86_64 >/dev/null 2>&1; then
+    echo "Error: qemu-system-x86_64 is not installed or is not in your PATH."
+    echo
+    echo "Install QEMU and try again."
+    exit 1
+fi
+
+if ! command -v cargo >/dev/null 2>&1; then
+    echo "Error: cargo is not installed or is not in your PATH."
+    echo
+    echo "Install CARGO and try again."
+    exit 1
+fi
+
+if [[ ! -f ./bios/OVMF.4m.fd ]]; then
+    echo "Error: ./bios/OVMF.4m.fd not found."
+    exit 1
+fi
+
 
 cargo build --release
 mkdir -p esp/EFI/BOOT
