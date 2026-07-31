28739b3a766fd319da462e41c013305a46fcae37
Guru901 <avinashkaur842@gmail.com>
2026-07-31 06:07:50 +0530

    Add CI\CD configuration

--- build.sh
+++ build.sh
@@ -24,4 +24,4 @@ fi
 cargo build --release
 mkdir -p esp/EFI/BOOT
 cp target/x86_64-unknown-uefi/release/agnostos.efi esp/EFI/BOOT/BOOTX64.EFI
-qemu-system-x86_64 -bios ./bios/OVMF.4m.fd -drive format=raw,file=fat:rw:esp
+exec qemu-system-x86_64 -bios ./bios/OVMF.4m.fd -drive format=raw,file=fat:rw:esp
